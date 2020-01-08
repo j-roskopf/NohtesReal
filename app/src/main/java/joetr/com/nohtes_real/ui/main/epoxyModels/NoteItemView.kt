30cc62d7d86da06c94bcb1950cdc6811ceb97942
josephroskopf <joebrothehobo@gmail.com>
2020-01-07 19:11:35 -0600

    labels on main page

--- app/src/main/java/joetr/com/nohtes_real/ui/main/epoxyModels/NoteItemView.kt
+++ app/src/main/java/joetr/com/nohtes_real/ui/main/epoxyModels/NoteItemView.kt
@@ -1,15 +1,20 @@
 package joetr.com.nohtes_real.ui.main.epoxyModels
 
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.github.irshulx.Editor
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import joetr.com.data.entities.NoteEntity
 import joetr.com.nohtes_real.R
 import joetr.com.nohtes_real.android.extensions.KotlinEpoxyHolder
 import joetr.com.nohtes_real.ui.main.MainActionHandler
 import joetr.com.nohtes_real.ui.main.MainPageAction
+import joetr.com.nohtes_real.ui.note.label.LabelBottomSheet
 
 @EpoxyModelClass(layout = R.layout.note_item)
 abstract class NoteItemView : EpoxyModelWithHolder<NoteItemViewHolder>() {
@@ -26,6 +31,26 @@ abstract class NoteItemView : EpoxyModelWithHolder<NoteItemViewHolder>() {
         holder.noteItemBaseLayout.setOnClickListener {
             actionHandler(MainPageAction.NoteClicked(noteEntity))
         }
+
+        addChips(holder)
+    }
+
+    private fun addChips(holder: NoteItemViewHolder) {
+        holder.noteItemChipGroup.removeAllViews()
+
+        noteEntity.labels?.forEach {
+            val chip = LayoutInflater.from(holder.noteItemChipGroup.context).inflate(
+                R.layout.add_note_chip,
+                holder.noteItemChipGroup,
+                false
+            ) as Chip
+
+            chip.text = it.label
+            chip.setOnClickListener {
+                actionHandler(MainPageAction.NoteClicked(noteEntity))
+            }
+            holder.noteItemChipGroup.addView(chip)
+        }
     }
 
     override fun unbind(holder: NoteItemViewHolder) {
@@ -37,4 +62,5 @@ abstract class NoteItemView : EpoxyModelWithHolder<NoteItemViewHolder>() {
 class NoteItemViewHolder : KotlinEpoxyHolder() {
     val noteItemEditor by bind<Editor>(R.id.noteItemEditor)
     val noteItemBaseLayout by bind<ConstraintLayout>(R.id.noteItemBaseLayout)
+    val noteItemChipGroup by bind<ChipGroup>(R.id.noteItemChipGroup)
 }
