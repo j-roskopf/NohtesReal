9c14ab279935118df872c0759db39a1713da0561
Joe <jroskopf14@alumni.uwosh.edu>
2020-02-09 08:26:41 -0600

    update  gradle

--- markdeditor/src/main/java/xute/markdeditor/components/HorizontalDividerComponent.java
+++ markdeditor/src/main/java/xute/markdeditor/components/HorizontalDividerComponent.java
@@ -2,7 +2,6 @@ package xute.markdeditor.components;
 
 import android.content.Context;
 
-//todo joe convert everything to kotlin
 //todo joe add support for theming via library
 
 public class HorizontalDividerComponent {
